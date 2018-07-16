e28b03c2c1ba20e624900152a0135d639b1f1676
berak <px1704@web.de>
2018-07-16 09:42:48 +0200

    9

--- wsgi.py
+++ wsgi.py
@@ -7,16 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-
+import os
 # only nessecary for running on paas platforms like heroku, or openshift
 def application(environ, start_response):
-    data = "helo."
+    data = b"helo."
     start_response("200 OK", [
             ("Content-Type", "text/html"),
             ("Content-Length", str(len(data)))
             ])
     return iter([data])
 
-#from wsgiref.simple_server import make_server
-#httpd = make_server( '0.0.0.0', int(os.environ.get("PORT", 9000)), application )
-#while True: httpd.handle_request()
+from wsgiref.simple_server import make_server
+httpd = make_server( '0.0.0.0', int(os.environ.get("PORT", 9000)), application )
+while True: httpd.handle_request()
